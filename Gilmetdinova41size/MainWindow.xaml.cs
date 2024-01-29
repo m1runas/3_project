6a3a1f3cae49052b11c2b6ba02b666ec6389413c
JeyBarrette <144144826+m1runas@users.noreply.github.com>
2024-01-29 18:16:15 +0500

    //17 лб

---
+++ Gilmetdinova41size/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gilmetdinova41size
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Page1());
+            Manager.MainFrame = MainFrame;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if(MainFrame.CanGoBack) {
+                btnback.Visibility = Visibility.Visible;
+                    }
+            else { btnback.Visibility = Visibility.Hidden; }
+        }
+    }
+}
---
+++ Gilmetdinova41size/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gilmetdinova41size
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            var currentProduct = Gilmetdinova41sizeEntities.GetContext().Product.ToList();
+            SizeListView.ItemsSource = currentProduct;
+        }
+    }
+}
